6c3e17331515e93f6b65a8a5dc2cef2dfbab942d
Vasily Shelkov <v.shelkov@iwoca.co.uk>
2019-09-08 01:01:53 +0100

    use axios hook

--- src/popup/payment-service-cards/index.tsx
+++ src/popup/payment-service-cards/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
+import useAxios from "@use-hooks/axios";
 import { connect } from "react-redux";
-import axios, { AxiosResponse } from "axios";
 import { RouteComponentProps, Router } from "@reach/router";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { RootState } from "typesafe-actions";
@@ -14,89 +14,72 @@ interface Props extends RouteComponentProps {
   addApiResponseToClientCache: typeof cacheActions.cacheResponse;
   cardsApiResponse: CardsApiResponse;
 }
-interface State {
-  loading: boolean;
-}
-export class PaymentServiceCards extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      loading: !Boolean(this.props.cardsApiResponse)
-    };
-  }
+const PaymentServiceCards = ({
+  cardsApiResponse,
+  addApiResponseToClientCache
+}: Props) => {
+  const { loading } = useAxios({
+    url: `http://localhost:8000/scrape-payment-service-test-cards`,
+    method: "GET",
+    filter: () => !cardsApiResponse,
+    customHandler: (error, response) =>
+      response && addApiResponseToClientCache(response.data, "stripe")
+  });
 
-  componentDidMount() {
-    if (!this.props.cardsApiResponse) {
-      axios
-        .get("http://localhost:8000/scrape-payment-service-test-cards")
-        .then((response: AxiosResponse) => {
-          this.props.addApiResponseToClientCache(response.data, "stripe");
-          this.setState({
-            loading: false
-          });
-        });
-    }
-  }
-
-  render() {
-    const { cardsApiResponse } = this.props;
-    const { loading } = this.state;
-
-    if (loading) {
-      return (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "10px"
-          }}
-        >
-          <CircularProgress size={50} />
-        </div>
-      );
-    }
-
+  if (loading) {
     return (
-      <>
-        <Router>
-          <CardTypeChooser path="/" />
-          {cardsApiResponse.cardResponses && (
-            <CardsList
-              path="/responses"
-              listTitle="HTTP RESPONSES"
-              cards={cardsApiResponse && cardsApiResponse.cardResponses}
-            />
-          )}
-
-          {cardsApiResponse.cardTypes && (
-            <CardsList
-              path="/card-types"
-              listTitle="CARD TYPES"
-              cards={cardsApiResponse && cardsApiResponse.cardTypes}
-            />
-          )}
-
-          {cardsApiResponse.internationalCards && (
-            <CardsList
-              path="/international-cards"
-              listTitle="INTERNATIONAL CARDS"
-              cards={cardsApiResponse && cardsApiResponse.internationalCards}
-            />
-          )}
-
-          {cardsApiResponse && cardsApiResponse["3dsCards"] && (
-            <CardsList
-              path="/3ds-cards"
-              listTitle="3DS CARDS"
-              cards={cardsApiResponse && cardsApiResponse["3dsCards"]}
-            />
-          )}
-        </Router>
-      </>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "10px"
+        }}
+      >
+        <CircularProgress size={50} />
+      </div>
     );
   }
-}
+
+  return (
+    <>
+      <Router>
+        <CardTypeChooser path="/" />
+        {cardsApiResponse.cardResponses && (
+          <CardsList
+            path="/responses"
+            listTitle="HTTP RESPONSES"
+            cards={cardsApiResponse && cardsApiResponse.cardResponses}
+          />
+        )}
+
+        {cardsApiResponse.cardTypes && (
+          <CardsList
+            path="/card-types"
+            listTitle="CARD TYPES"
+            cards={cardsApiResponse && cardsApiResponse.cardTypes}
+          />
+        )}
+
+        {cardsApiResponse.internationalCards && (
+          <CardsList
+            path="/international-cards"
+            listTitle="INTERNATIONAL CARDS"
+            cards={cardsApiResponse && cardsApiResponse.internationalCards}
+          />
+        )}
+
+        {cardsApiResponse && cardsApiResponse["3dsCards"] && (
+          <CardsList
+            path="/3ds-cards"
+            listTitle="3DS CARDS"
+            cards={cardsApiResponse && cardsApiResponse["3dsCards"]}
+          />
+        )}
+      </Router>
+    </>
+  );
+};
 
 const dispatchProps = {
   addApiResponseToClientCache: cacheActions.cacheResponse
